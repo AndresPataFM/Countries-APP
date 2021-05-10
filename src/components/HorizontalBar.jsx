3246f4a769107ceffc5fca55ba0c748d2604278f
Natalia Verón <80424181+nativeron@users.noreply.github.com>
2021-05-10 17:00:37 -0300

    Update HorizontalBar.jsx

--- src/components/HorizontalBar.jsx
+++ src/components/HorizontalBar.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from './Card.jsx';
 //Al importar Card se importa a la vez link?
 import styles from './HorizontalBar.module.css';
+import ScrollMenu from 'react-horizontal-scrolling-menu';
+import { Link } from 'react-router-dom';
 
-export default function Cards(){
+export default function Cards({countries}){
     return (
-        <div >
-            codigo
+        <div className={styles.container}>
+            <h2>Countries</h2>
+            <div className={styles.bar}>
+                <ScrollMenu
+                    wheel= {false}
+                    scrollBy={3}
+                    arrowLeft={<div className={styles.arrow}>{"🡠"}</div>}
+                    arrowRight={<div className={styles.arrow}>{"🡢"}</div>}
+                    data={countries.map((countries, index) => (
+                            <Link className={styles.clase2} to={`/home/${countries.name}`}>
+                            <Card 
+                                name={countries.name}
+                                flag={countries.flag}
+                                subregion={countries.subregion}
+                                key={index}
+                            />
+                            </Link>
+                    ))}
+                />
+            </div>
+            
         </div>
+        
     );
 };
+// tengo q hacer "npm install --save react-horizontal-scrolling-menu "
