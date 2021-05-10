efb151434785248334c25c2673d30e590b828f7c
Natalia Verón <80424181+nativeron@users.noreply.github.com>
2021-05-10 16:55:48 -0300

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import SearchBar from './SearchBar.jsx';
 import './Nav.css';
 import image from '../panda.png'
+import { Link } from 'react-router-dom';
 
-export default function Nav(){
+export default function Nav({onSearch}){
     return(
-       <nav><span id="conociendo">conociendo</span> <span id="asia">Asia</span>
-        <img src={image} width="95" />
-        
+       <nav id='cnt'>
+        <div>
+            <span id="conociendo">Exploring</span><span id="asia">Asia</span>
+           <Link to={"/"}><img src={image} width="95" /></Link>
+        </div>
+        <SearchBar
+            onSearch={onSearch}
+        />
        </nav>
     );
    
