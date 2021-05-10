c3392aea96ab0854b51ed9f9c2b91afdfee167d6
Natalia Verón <80424181+nativeron@users.noreply.github.com>
2021-05-10 16:57:40 -0300

    Update Landing.jsx

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,48 +3,60 @@ import './Landing.css';
 import image1 from "../gate.png"
 import image2 from "../montanas.png"
 import image3 from "../teamwork.png"
-import image4 from "../asian.jpg"
-
+import image4 from "../asian.png"
+import { Link } from 'react-router-dom';
+import Form from './Form.jsx'
+import image from '../panda.png'
 export default function Landing(){
     return(
         
         <div id="landing">
+             <div id="logo">
+            <span id="conociendo">Exploring</span><span id="asia">Asia</span>
+           <Link to={"/"}><img src={image} width="95" /></Link>
+        </div>
             <div id="container">
                 <div id="info">
-                    <p>Conociendo Asia te permitirá 
-                        conocer información importante de los
-                         países que conforman este continente.
-                         Ingresa ahora y aprende un poco más sobre 
-                         una de las culturas más antiguas de la historia</p>
-                         <button>Ingresar Ahora</button>
+                    <p>Exploring Asia will let you learn important 
+                         information about the countries that make 
+                         up this continent. Enter now and learn more 
+                         about one of the oldest amalgamation of culture
+                         of the human history.</p>
+                         <Link to={`/home`}>
+                         <button>Enter Now</button>
+                         </Link>
                 </div>
                 <div id="datos">
                 
                 <img src={image4}/>
-                <h1>Datos de interés</h1>
-                <p> Un continente con tanta historia solo tiene cosas buenas que contar</p>
+                <h1>Data of interest</h1>
+                <p> A continen with so much history only has good things to tell</p>
                 </div>
+                <Form/>
                
             </div>
             <div class= "cont" id="cont">
             <div id="poblacion">
-                <img src={image3}/>
-                <h4>Cuanta gente!</h4>
-                <p>Asia es el continente más <b>poblado</b> del 
-                planeta , 4.46 millones de personas viven en 
-                44.5 millones de kilómetros cuadrados.</p>
+                <img id="imagen2" src={image3}/>
+                <h4>How many people!</h4>
+                <p>Asia is the continent with the most
+                <b> population density</b> of the entire 
+                planet, 4.46 millions of people live in
+                44.5 million square kilometers.
+                </p>
                 </div>
             <div id="everest">
-                <img src={image2}/>
-                <h4>De altura</h4>
-                <p>El monte <b>Everest</b> es la montaña más alta del mundo con una
-                altitud de 8.848 metros, equivalentes a 80campos de fútbol.</p>
+                <img id="imagen1" src={image2}/>
+                <h4>Height</h4>
+                <p>Mount <b>Everest</b> is the highest mountain in the world
+                with an altitude of 8.848 meters, equivalent to 80 futbol satiums.
+                </p>
             </div>
             <div id="ciudad">
-                <img src={image1}/>
-                <h4>Ciudad</h4>
-                <p>Con 13 millones de habitantes, <b>Tokio</b> se lleva el premio
-                a la ciudad más poblada de toda Asia.</p>
+                <img id="imagen" src={image1}/>
+                <h4>Cities</h4>
+                <p>With over 13 million habitants <b>Tokio</b> takes the prize
+                for the most populated city in all of Asia.</p>
             </div>
             </div>
 
