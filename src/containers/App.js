fc2bb951640cedad1e5879c848d1d8bea5b418c7
Natalia Verón <80424181+nativeron@users.noreply.github.com>
2021-05-10 16:53:44 -0300

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,7 @@
 //Estado y React
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+// //Instalar axios con 'npm install axios'
+// import axios from 'axios';
 //Componentes
 import Nav from '../components/Nav.jsx';  //Tiene a SearchBar
 import HorizontalBar from '../components/HorizontalBar.jsx'; //Tiene a Card
@@ -7,49 +9,100 @@ import Descripcion from '../components/Descripcion.jsx';
 import Country from '../components/Country.jsx';
 import Landing from '../components/Landing.jsx';
 //imports de componentes que luego se borran
-import TestCountry from '../components/TestCountry'; //Data sacada del API para testear
-import Card from '../components/Card.jsx';
+import TestCountry, { Afghanistan } from '../components/TestCountry'; //Data sacada del API para testear
 import SearchBar from '../components/SearchBar.jsx';
 //Ruta
 import { Route } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 //Estilo
-import './App.css'
+import './App.css';
+
 
 
 
 // Primer funcion solo para testear los componentes, sabemos que aparece en el div root
 export default function App(){
   //aca iria la constante de estado
+  const [mainCountry, setMainCountry] = useState(Afghanistan)
+  const [countriesData, setCountriesData] = useState(TestCountry);
 
   //aca va el llamado a la api
+  const apiCountries = 'https://restcountries.eu/rest/v2/region/asia';
+
+  //fetch de TODA la data
+  //fetch sincrónico
+  useEffect(() => {
+    fetch(apiCountries)
+    .then(res => res.json())
+    .then(json => {
+      var data = json
+      console.log(data)
+      setCountriesData(data)
+      setMainCountry(data[2])}) 
+  }, []) 
+
+  function onSearch(country) {
+    //Llamado a la API del clima
+    fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+      .then(r => r.json())
+      .then((recurso) => {
+        if(recurso[0] !== undefined){
+          setMainCountry(recurso[0])
+        } else {
+          alert("Country not found");
+        }
+      });
+  }
+
+  const { ctryName } = useParams()
+  // con el [] solo hace el llamado una vez, si esta afuera llama siempre
+ 
+  //fetch asincrónico
+  // useEffect(() => {
+  //   const getCountriesData = async (apiCountries)=>{
+  //     let res = await fetch(apiCountries),
+  //     json = await res.json();
+  //     console.log(json)
+  //     setCountriesData(json);
+  //     setMainCountry(countriesData[0])
+  //   }
+  // }, [])
+  
+  //Fetch de click
 
   //aca retorna los componentes
   //solo puede retornar 1 componente pero este puede tener muchos hijos
   return (
     <div className="App">
-      <div>
-        <Nav/>
-      </div>
-      <div>
-        <SearchBar/>
-      </div>
-      <div>
-        <Card/>
-      </div>
-      <div>
-        <HorizontalBar/>
-      </div>
-      <div>
-        <Country/>
-      </div>
-      <div>
-        <Landing/>
-      </div>
-      <div>
-        <Descripcion/>
-      </div>
+
+
+        <Route
+          exact path= '/'
+          component={Landing}
+        />
+        <Route
+          path = '/home'
+          render={()=><Nav onSearch={onSearch}/>}
+        
+        />
+        <Route
+          path= '/home'
+          render={()=><Country
+            country={mainCountry}
+            />}
+        />
+        <Route
+          path= '/home'
+          render={()=><HorizontalBar
+            countries={countriesData}
+          />}
+        />
+       
+
+        
     </div>
-  );
+     
+  ); 
 };
 
 // Función funcional
@@ -88,3 +141,24 @@ export default function App(){
 // }
 
 // export default App;
+
+// return (
+//   <div className="App">
+//       <Nav/>
+//       <SearchBar/>
+//       <Country
+//           country={mainCountry}
+//       />
+      // <HorizontalBar
+      //   countries={countriesData}
+      // />
+//       <Landing/>
+//       <Descripcion/>
+//   </div>
+// );
+//<Route
+//path= '/home/:ctryName'
+//render={()=>{<Country
+//  country={onSearch(ctryName)}
+//  />}}
+///>
