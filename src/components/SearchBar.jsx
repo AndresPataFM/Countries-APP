8827afa68738ca12e2fa1ec47b536d3fb5d17de0
Natalia Verón <80424181+nativeron@users.noreply.github.com>
2021-05-10 17:07:58 -0300

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,10 +2,20 @@ import React, { useState } from "react";
 // lo necesita porque usamos el state para hacer la busqueda
 import styles from './SearchBar.module.css'
 
-export default function SearchBar(){
-    return(
-        <div>
-            codigo
-        </div>
+export default function SearchBar({onSearch}) {
+    const [country, setCountry] = useState("");
+    return (
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        onSearch(country);
+      }}className={StyleSheet.searchBar}>
+        <input
+          type="text"
+          placeholder="Country..."
+          value={country}
+          onChange={e => setCountry(e.target.value)}
+          className={styles.searchInput}/>
+        <input type="submit" value="Search" className={styles.searchBtn}/>
+      </form>
     );
-};
+  }
